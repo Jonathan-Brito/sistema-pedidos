07638edf91c66f40dd764c946c7d7301a2f8480a
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-07-30 20:02:52 -0300

    Testando category

--- src/main/java/com/brito/sistemapedidos/resources/CategoryResource.java
+++ src/main/java/com/brito/sistemapedidos/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.brito.sistemapedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brito.sistemapedidos.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcioando perfeitamente!!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
