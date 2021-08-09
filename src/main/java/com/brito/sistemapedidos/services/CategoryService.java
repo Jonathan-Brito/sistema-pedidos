f8b9a8f4ebc8a0b845cdeea248223af5e5dacac3
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-08-09 16:44:11 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/brito/sistemapedidos/services/CategoryService.java
+++ src/main/java/com/brito/sistemapedidos/services/CategoryService.java
@@ -7,6 +7,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brito.sistemapedidos.dtos.CategoryDTO;
@@ -57,6 +60,11 @@ public class CategoryService {
 			throw new DataIntegrityException
 			("Category não pode ser deletado! Possui products associados");
 		}
+	}
+	
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoryRepository.findAll(pageRequest);
 	}
 
 }
