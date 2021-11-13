028ff59cef39407280f03278498b799a43567f2e
Jonathan-Brito <devjonathanbrito@gmail.com>
2021-11-13 18:19:45 -0300

    Implementando autorização

--- src/main/java/com/brito/sistemapedidos/config/SecurityConfig.java
+++ src/main/java/com/brito/sistemapedidos/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.brito.sistemapedidos.security.JWTAuthenticationFilter;
+import com.brito.sistemapedidos.security.JWTAuthorizationFilter;
 import com.brito.sistemapedidos.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
